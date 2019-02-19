3597d4ef5d909a462f5b0c76331d0d2810c1b334
Endu <endaldemi@gmail.com>
2019-02-19 11:17:09 -0500

    check my work

--- stats/attendance.py
+++ stats/attendance.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 
 from data import games
 
-attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year', 'multi3']]
+attendance = games.loc[(games['type'] == 'info') & (games['multi2'] ==
+'attendance'), ['year', 'multi3']]
 attendance.columns = ['year', 'attendance']
 
-attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
+attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:,
+'attendance'])
 attendance.plot(x='year', y='attendance', figsize=(15,7), kind='bar')
 
 plt.xlabel('Year')
